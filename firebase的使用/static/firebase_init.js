8cd04fc003af76d1890a08ae55db4dd6fc5588dd
auxo <auxo_86@hotmail.com>
2017-09-19 21:34:02 +0800

    flask應用-使用firebase ui for WEB登入

---
+++ firebase的使用/static/firebase_init.js
@@ -0,0 +1,12 @@
+//起始firebase
+// Initialize Firebase
+//從firebase摳來
+var config = {
+  apiKey: "AIzaSyAIcIb1BZw78726LmjqJJamYsAhEaIfces",
+  authDomain: "cool-cf19a.firebaseapp.com",
+  databaseURL: "https://cool-cf19a.firebaseio.com",
+  projectId: "cool-cf19a",
+  storageBucket: "cool-cf19a.appspot.com",
+  messagingSenderId: "145780310346"
+};
+firebase.initializeApp(config);
